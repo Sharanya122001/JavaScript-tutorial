f1ce60337af0e5505e19540ae5bb8482f3f53ab7
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-02 12:31:52 +0530

    scope 01

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,11 @@
+let a=10;
+var b=9;
+const r=5;
+
+
+//{}// this is called scope
+// what we  are declaring in the global can go inside the scoe,
+// but what we are declaring in the scope should not go out of the scope
+console.log(a);
+console.log(b);
+console.log(r);
