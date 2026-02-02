5ebdd7ef39e850fa306a4f0d46a0075d6a3e764f
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-02 12:56:11 +0530

    this is scope 02

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -9,3 +9,33 @@ const r=5;
 console.log(a);
 console.log(b);
 console.log(r);
+
+
+// scope part 2
+//nested function
+
+function one(){
+    const username="sharanya"
+    function two(){
+        const output="website"
+        console.log(username);
+    }
+    //console.log(output) // this is taking from the inside function,scope limits here
+    two()
+}
+one()
+//when there is a nested function child can access only the parent variables not vicevera
+
+//hosting means moving the declaration to the top during execution
+
+//console.log(s(4,5));
+function s(a,b){
+    return a+b;
+}
+
+const sum=function(a1,b1)
+{
+    return a1+b1;
+}
+console.log(sum(2,3))
+
