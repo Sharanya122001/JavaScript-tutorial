4d9f4b7268a8d2fda25cb76c32ad5000dbe281f2
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-02 14:53:13 +0530

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+// this arrow function comes input picture in 2015 in the ES6
+// this keyword refer to the current context
+
+const user={
+    username:"sharnya",
+    price:999,
+
+    welcome : function(){
+        console.log(`${this.username}, hi how r u`)
+        console.log(this)
+    }
+
+}
+user.welcome()
+user.username="sam"
+user.welcome()
+
+console.log(this)
+///imp note: in browser the global object is windows object
+
+
+function one(){
+    username:"sharanay"
+    console.log(this.username)
+}
+one()
+
+
+const two=()=>{
+    let username="shatanau"
+    console.log(this)
+}
+two()
+
+//arrow function
+const a=(b,c)=>{
+    return b+c
+}
+
+// we can see that we have the function with the {} and return in it
+// so when ever we are using the {} then return is must with it if we dont use the return then
+// code will be like this
+const b=(y,m)=> y+m
+// we can add the () for the y+m 
+//const b=(y,m)=>(y+m) ........ this is implicit
