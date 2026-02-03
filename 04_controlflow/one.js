0edd092868ab8f50f080bce648e6af23a2be87db
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-03 12:24:06 +0530

    if statement

---
+++ 04_controlflow/one.js
@@ -0,0 +1,20 @@
+//if
+const temp=41
+if(temp==40){
+   console.log("less than")
+}
+else{
+    console.log("more than")
+}
+console.log("done")
+
+
+const score =200;
+if(score >100){
+    let power="fly"
+    console.log(`use ${power}`)
+}
+//console.log(`use ${power}`)
+
+const balance=100
+//if(balance>50) console.log("good"),console.log("bad");--bad pratice of writing the code
