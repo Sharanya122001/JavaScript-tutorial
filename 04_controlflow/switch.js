9de4e0d5d9c467055e839f4d5b6fda631276ec1f
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-03 12:32:12 +0530

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,47 @@
+//basci syntx of the switch
+// switch(key){
+//     case value:
+
+//       break;
+//     default:
+//         break;
+// }
+
+// duplicating use the kry shift + alt + downarrow
+const month=6;
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("sept");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    default:
+        console.log("dec");
+        break;
+}
+// if we dont use the break then it will execute all other cases but no the default
