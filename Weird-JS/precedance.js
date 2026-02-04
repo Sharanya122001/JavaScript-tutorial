7021c16dc1559f73b1fa0f336fc606f77dc27ca9
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-04 12:31:19 +0530

    assignment operators

---
+++ Weird-JS/precedance.js
@@ -0,0 +1,12 @@
+//precedance and associativity
+
+var a=7
+var b=5
+var c=8
+
+a=b=c;
+console.log(a)
+console.log(b)
+console.log(c)
+// we can see that the assignment operator has right-to-left associativity
+//so we are getting the values as value of c
