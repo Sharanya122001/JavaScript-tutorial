371b885e98cb7e320df984ee58f177ea03802337
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-04 15:21:14 +0530

    or operator

--- Weird-JS/precedance.js
+++ Weird-JS/precedance.js
@@ -10,3 +10,7 @@ console.log(b)
 console.log(c)
 // we can see that the assignment operator has right-to-left associativity
 //so we are getting the values as value of c
+
+//if the case is something like this if the operator is || between two then
+//console.log(null || "hello") then the output will be the hello
+// same in case of the the 0||1 output will be 1
