6013398bff08cab12f13c5f99bf9e2e54cad7b4e
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-18 14:42:55 +0530

    classes

--- 13_classes_and_oops/classes.js
+++ 13_classes_and_oops/classes.js
@@ -34,3 +34,117 @@ class Teacher extends  User{
         this.password=password
     }
 }
+//normal method in the class
+class person{
+    info(){
+        console.log("this is the info")
+    }
+}
+const p2=new person();
+p2.info()
+
+//static method in the class
+class personone{
+    static info(){
+        console.log("this is the info")
+    }
+}
+personone.info()
+//direct call with the static method without creating the object
+
+// Easy rule:
+// Normal method → use object
+// Static method → use class name
+
+
+//handling errrors
+class calculator{
+    divide(a,b){
+        if(b==0){
+            throw new Error("cannot divide by zero")//throw creates the error
+        }
+    return a/b; 
+    }
+}
+
+const cal=new calculator();
+//try catch handles the error safely
+try{ 
+    console.log(cal.divide(10,0));
+}catch(error){
+    console.log(error.message);
+}
+
+
+//inheritance
+class animal{
+    constructor(name){
+        this.name=name;
+    }
+
+    speak(){
+        console.log("animal sound")
+    }
+}
+
+class dog extends animal{
+    constructor(name,bread){
+        super(name),
+        this.bread=bread
+    }
+
+    speak(){
+        console.log("dog barks")
+    }
+}
+const d1=new dog("lab","goldenretrevier")
+console.log(d1)
+// What is super()?
+// Calls parent constructor
+// Must be called before using this
+// Required in child class constructor
+
+
+// class Persontwo {
+//   greet() {
+//     console.log("Hi");
+//   }
+// }
+// // Behind the scenes:
+// Person.prototype.greet = function() {
+//   console.log("Hi");
+// }
+
+class userone{
+    constructor(username,email,password){
+        this.username=username,
+        this.email=email,
+        this.password=password
+    }
+
+    login(){
+        console.log("loggedin")
+    }
+    logout(){
+        console.log("loggedout")
+    }
+}
+const p5=new userone("shar","chat@b","1234")
+p5.login();
+
+
+class helper{
+    constructor(number){
+        this.number=number
+    }
+    squre(){
+        return this.number*this.number  
+    }
+    static add(a,b){
+        return a+b;
+    }
+}
+
+const obj=new helper(5);
+console.log(obj.squre());
+console.log(helper.add(2,3))
